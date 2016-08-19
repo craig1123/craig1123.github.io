14dba75b783ec62c066f6e4f2209024ea4df708a
craig1123 <craigwalker1123@gmail.com>
2016-08-19 17:52:48 -0600

    hosting

---
+++ gulpfile.babel.js
@@ -0,0 +1,47 @@
+import gulp from 'gulp';
+import concat from 'gulp-concat';
+import annotate from 'gulp-ng-annotate';
+import plumber from 'gulp-plumber';
+import uglify from 'gulp-uglify';
+import watch from 'gulp-watch';
+import sass from 'gulp-sass';
+import path from 'path';
+import babel from 'gulp-babel';
+
+const paths = {
+  jsSource: ['./public/js/*.js'],
+  sassSource: ['./public/styles/*.scss'],
+  paths: [ path.join(__dirname, 'styles') ]
+};
+const sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded',
+  paths: [ path.join(__dirname, 'styles') ]
+};
+
+gulp.task('js', () =>  {
+  return gulp.src(paths.jsSource)
+  .pipe(plumber())
+  .pipe(babel({
+    presets: ["es2015"]
+  }))
+  .pipe(concat('bundle.js'))
+  .pipe(annotate())
+  .pipe(uglify())
+  .pipe(gulp.dest('./public'));
+});
+
+
+gulp.task('sass', () => {
+  return gulp.src(paths.sassSource)
+  .pipe(sass(sassOptions).on('error', sass.logError))
+  .pipe(concat('style.css'))
+  .pipe(gulp.dest('./public/styles'));
+});
+
+gulp.task('watch', () =>  {
+  gulp.watch(paths.jsSource, ['js']);
+  gulp.watch(paths.sassSource, ['sass']);
+});
+
+gulp.task('default', ['watch', 'js', 'sass']);
