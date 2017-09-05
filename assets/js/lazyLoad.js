0f93dd0ebe658f1a43f669c451e7c2bf4cb2cc8b
Craig Walker <cwalker@overstock.com>
2017-09-05 10:47:55 -0600

    deleting service worker, lazy-load working

---
+++ assets/js/lazyLoad.js
@@ -0,0 +1,23 @@
+var lazyImgs = [].slice.call(document.querySelectorAll('.lazy-load'));
+if ('IntersectionObserver' in window) {
+    var config = {
+        rootMargin: '50px 0px',
+        threshold: 0.01,
+    };
+    var onChange = function(changes) {
+        changes.forEach(function(change) {
+            if (change.intersectionRatio > 0) {
+                change.target.src = change.target.dataset.src;
+                observer.unobserve(change.target);
+            }
+        });
+    };
+    var observer = new IntersectionObserver(onChange, config);
+    lazyImgs.forEach(function(img) {
+        observer.observe(img)
+    });
+} else {
+    lazyImgs.forEach(function(image) {
+        image.src = image.dataset.src;
+    });
+}
