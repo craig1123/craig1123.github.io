e0ea248d566cf2888f89644f220af3bab1072701
Craig Walker <craigwalker1123@gmail.com>
2017-10-31 16:00:26 -0600

    taking out all iframes, comments, compressing things for faster performance

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,4 @@
-
-
 (function($) {
-
 	skel
 		.breakpoints({
 			xlarge:	'(max-width: 1680px)',
@@ -10,25 +7,18 @@
 			small:	'(max-width: 736px)',
 			xsmall:	'(max-width: 480px)'
 		});
-
 	$(function() {
-
 		var	$window = $(window),
 			$body = $('body'),
 			$wrapper = $('#page-wrapper'),
 			$banner = $('#banner'),
 			$header = $('#header');
-
-		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
-
 			$window.on('load', function() {
 				window.setTimeout(function() {
 					$body.removeClass('is-loading');
-				}, 100);
+				}, 0);
 			});
-
-		// Mobile?
 			if (skel.vars.mobile)
 				$body.addClass('is-mobile');
 			else
@@ -39,58 +29,33 @@
 					.on('+medium', function() {
 						$body.addClass('is-mobile');
 					});
-
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
-
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
-
-		// Scrolly.
 			$('.scrolly')
 				.scrolly({
 					speed: 1500,
 					offset: $header.outerHeight()
 				});
-
-		// Header.
 			if (skel.vars.IEVersion < 9)
 				$header.removeClass('alt');
-
 			if ($banner.length > 0
 			&&	$header.hasClass('alt')) {
-
 				$window.on('resize', function() { $window.trigger('scroll'); });
-
 				$banner.scrollex({
 					bottom:		$header.outerHeight() + 1,
 					terminate:	function() { $header.removeClass('alt'); },
 					enter:		function() { $header.addClass('alt'); },
 					leave:		function() { $header.removeClass('alt'); }
 				});
-
 			}
-
-			// show/hide timeline event, highlight square, based on which square is clicked
 			$('.timeline-circle').on('click', function() {
-
 				if (!$(this).hasClass('.current-square')) {
 					$('.current-square').removeClass('current-square');
 					$('.timeline-event.active-event').removeClass('active-event').hide();
 					$('.main-bio-text').removeClass('main-bio-text-active').hide();
-
 					$(this).addClass('current-square');
 					$('.main-bio-text').fadeIn(600).addClass('main-bio-text-active');
 					var target = $(this).attr('data-target');
 					$('#' + target).fadeIn(600).addClass('active-event');
 				}
 			});
-
 	});
-
 })(jQuery);
